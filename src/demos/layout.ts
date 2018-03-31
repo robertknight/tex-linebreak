6ac4b51ad54295f6050682617c8abbdba2b6978e
Robert Knight <robertknight@gmail.com>
2018-03-31 10:13:27 +0100

    Add a demo that renders justified text as span elements
    Add a demo that shows rendering justified text as span elements, instead
    of to a canvas.
    
    Also update the styling of the input box to match the rendered output so
    it is easier to compare the browser's ragged-right rendering with the
    justified output from the Knuth-Plass algorithm.

--- src/demos/layout.ts
+++ src/demos/layout.ts
@@ -5,6 +5,9 @@
 
 import { layoutItemsFromString, layoutParagraph, TextBox, MAX_COST, MIN_COST } from '../layout';
 
+/**
+ * Render a string as justified text into a `<canvas>`.
+ */
 function renderText(c: HTMLCanvasElement, t: string) {
   const ctx = canvas.getContext('2d')!;
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -33,8 +36,59 @@ function renderText(c: HTMLCanvasElement, t: string) {
   });
 }
 
+/**
+ * Render a string as justified text using `<span>` and `<br>` elements.
+ */
+function renderSpans(el: HTMLElement, t: string) {
+  // Clear element and measure font.
+  el.innerHTML = '';
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d')!;
+  const { fontSize, fontFamily, width, paddingLeft, paddingRight } = window.getComputedStyle(el);
+  const lineWidth = parseFloat(width!);
+  ctx.font = `${fontSize} ${fontFamily}`;
+
+  // Layout text.
+  const items = layoutItemsFromString(t, w => ctx.measureText(w).width);
+  const boxPositions = layoutParagraph(items, lineWidth, {
+    maxAdjustmentRatio: 1,
+    looseness: 0,
+    chlPenalty: 0,
+  });
+
+  // Generate `<div>` and `<span>` elements.
+  const addLine = () => {
+    const lineEl = document.createElement('div');
+    lineEl.style.whiteSpace = 'nowrap';
+    el.appendChild(lineEl);
+    return lineEl;
+  };
+
+  let prevXOffset = 0;
+  let lineEl = addLine();
+
+  boxPositions.forEach((bp, i) => {
+    const isNewLine = i > 0 && bp.line !== boxPositions[i - 1].line;
+    if (isNewLine) {
+      // In theory we could use `<br>` elements to insert line breaks, but in
+      // testing this resulted in Firefox and Chrome inserting an extra break
+      // near the end of the line. Adding lines this way produces consistent
+      // output across browsers.
+      lineEl = addLine();
+      prevXOffset = 0;
+    }
+    const span = document.createElement('span');
+    const item = items[bp.box];
+    span.textContent = (item as TextBox).text;
+    span.style.marginLeft = `${bp.xOffset - prevXOffset}px`;
+    prevXOffset = bp.xOffset + ctx.measureText(span.textContent).width;
+    lineEl.appendChild(span);
+  });
+}
+
 const textarea = document.querySelector('textarea')!;
 const canvas = document.querySelector('canvas')!;
+const para = document.querySelector('.output-p')! as HTMLElement;
 
 // Setup canvas for high DPI displays.
 const ctx = canvas.getContext('2d')!;
@@ -48,5 +102,7 @@ ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
 ctx.font = '13pt sans-serif';
 textarea.addEventListener('input', () => {
   renderText(canvas, textarea.value);
+  renderSpans(para, textarea.value);
 });
 renderText(canvas, textarea.value);
+renderSpans(para, textarea.value);
