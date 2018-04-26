30d0920e30add652e888c0d13ce145ec467a5b68
Robert Knight <robertknight@gmail.com>
2018-04-26 07:55:14 +0200

    Export hypher wrapper
    The 'hypher' library is not currently packaged as a UMD or for direct
    consumtion in the browser without jQuery. Since hyphenation is a
    critical part of rendering justified text, even with paragraph-level
    linebreaking, it makes sense to include it as a core feature.
    
    Also create a UMD bundle for the en-us patterns so that tex-linebreak
    can be conveniently used from the browser with a couple of script tags
    using:
    
    ```html
    <script src=".../dist/lib.js"></script>
    <script src=".../dist/hyphens_en-us.js"></script>
    <script>
    const paragraphs = Array.from(document.querySelectorAll('p'));
    const hyphenate = texLineBreak_lib.createHyphenator(texLineBreak_hyphens_en_us);
    texLineBreak_lib.justifyContent(paragraphs, hyphenate);
    </script>
    ```

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './layout';
 export * from './helpers';
 export * from './html';
+export * from './hyphenate';
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
 
+const hyphenLangs = [
+  'en-us',
+];
+let hyphenLibs = {};
+for (let lang of hyphenLangs) {
+  hyphenLibs[`hyphens_${lang}`] = `hyphenation.${lang}`;
+}
+
 module.exports = {
   entry: {
     demos: './src/demos/layout.ts',
     lib: './src',
+    ...hyphenLibs,
   },
   devtool: 'cheap-source-map',
   module: {
@@ -23,7 +32,7 @@ module.exports = {
     // Build a UMD bundle that can be used from a `<script>` tag, or imported
     // into a CommonJS / ESM environment.
     libraryTarget: 'umd',
-    library: 'texLineBreak',
+    library: 'texLineBreak_[name]',
 
     // Make the UMD bundle usable in Node.
     // See https://github.com/webpack/webpack/issues/6522
