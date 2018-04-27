74840854248e34a60234d3a81968edf178c81976
Robert Knight <robertknight@gmail.com>
2018-04-27 15:47:46 +0200

    Optimize `textNodesInRange`
    Rewrite and simplify `textNodesInRange`. Reduce the number of range
    operations performed and return from the function as soon as we
    encounter the last node in the range.

--- src/range.ts
+++ src/range.ts
@@ -1,21 +1,5 @@
-function isNodeInRange(range: Range, node: Node) {
-  if (node === range.startContainer || node === range.endContainer) {
-    return true;
-  }
-
-  const nodeRange = node.ownerDocument.createRange();
-  nodeRange.selectNode(node);
-  const isAtOrBeforeStart = range.compareBoundaryPoints(Range.START_TO_START, nodeRange) <= 0;
-  const isAtOrAfterEnd = range.compareBoundaryPoints(Range.END_TO_END, nodeRange) >= 0;
-  nodeRange.detach();
-  return isAtOrBeforeStart && isAtOrAfterEnd;
-}
-
-function forEachNodeInRange(r: Range, callback: (n: Node) => any) {
-  const root = r.commonAncestorContainer;
-
-  // The `whatToShow`, `filter` and `expandEntityReferences` arguments are
-  // mandatory in IE although optional according to the spec.
+export function textNodesInRange(range: Range) {
+  const root = range.commonAncestorContainer;
   const nodeIter = root.ownerDocument.createNodeIterator(
     root,
     NodeFilter.SHOW_ALL,
@@ -24,35 +8,22 @@ function forEachNodeInRange(r: Range, callback: (n: Node) => any) {
   );
 
   let currentNode;
+  let foundStart = false;
+  let nodes: Text[] = [];
   while ((currentNode = nodeIter.nextNode())) {
-    if (isNodeInRange(r, currentNode)) {
-      callback(currentNode);
+    if (!foundStart) {
+      if (currentNode !== range.startContainer) {
+        continue;
+      } else {
+        foundStart = true;
+      }
+    }
+    if (currentNode instanceof Text) {
+      nodes.push(currentNode);
+    }
+    if (currentNode === range.endContainer) {
+      break;
     }
   }
-}
-
-export function textNodesInRange(range: Range) {
-  const textNodes: Text[] = [];
-  forEachNodeInRange(range, node => {
-    if (node.nodeType === Node.TEXT_NODE) {
-      textNodes.push(node as Text);
-    }
-  });
-
-  return textNodes.filter(node => {
-    const nodeRange = node.ownerDocument.createRange();
-    nodeRange.selectNodeContents(node);
-    if (node === range.startContainer) {
-      nodeRange.setStart(node, range.startOffset);
-    }
-    if (node === range.endContainer) {
-      nodeRange.setEnd(node, range.endOffset);
-    }
-    if (nodeRange.collapsed) {
-      // If the range ends at the start of this text node or starts at the end
-      // of this node then do not include it.
-      return false;
-    }
-    return true;
-  });
+  return nodes;
 }
